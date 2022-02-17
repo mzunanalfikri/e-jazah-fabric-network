4d59d5930e3031ea23678847f98db79a16d1edb5
mzunanalfikri <zunanalfikri@gmail.com>
2022-02-17 22:46:13 +0700

    chaincode ready to operate

---
+++ query-example.sh
@@ -0,0 +1,10 @@
+
+export FABRIC_CFG_PATH=$PWD/config/
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer0.org1.e-jazah.id/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.e-jazah.id/users/Admin@org1.e-jazah.id/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.e-jazah.id --tls --cafile ${PWD}/organizations/ordererOrganizations/e-jazah.id/orderers/orderer.e-jazah.id/msp/tlscacerts/tlsca.e-jazah.id-cert.pem -C main-channel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer0.org1.e-jazah.id/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer1.org1.e-jazah.id/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
