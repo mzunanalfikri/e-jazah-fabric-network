fa8b0fa1af3c900b7f9e9eca24e37b1e19db7fa0
mzunanalfikri <zunanalfikri@gmail.com>
2022-04-04 10:00:26 +0700

    update chaincode

--- query-example.sh
+++ query-example.sh
@@ -7,7 +7,7 @@ export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.e
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.e-jazah.id/users/Admin@org1.e-jazah.id/msp
 export CORE_PEER_ADDRESS=localhost:7051
 
-# peer chaincode invoke -o localhost:8050 --ordererTLSHostnameOverride orderer2.e-jazah.id --tls --cafile ${PWD}/organizations/ordererOrganizations/e-jazah.id/orderers/orderer2.e-jazah.id/msp/tlscacerts/tlsca.e-jazah.id-cert.pem -C main-channel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer0.org1.e-jazah.id/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer1.org1.e-jazah.id/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
+peer chaincode invoke -o localhost:8050 --ordererTLSHostnameOverride orderer2.e-jazah.id --tls --cafile ${PWD}/organizations/ordererOrganizations/e-jazah.id/orderers/orderer2.e-jazah.id/msp/tlscacerts/tlsca.e-jazah.id-cert.pem -C main-channel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer0.org1.e-jazah.id/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer1.org1.e-jazah.id/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
 
 # peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.e-jazah.id --tls --cafile ${PWD}/organizations/ordererOrganizations/e-jazah.id/orderers/orderer.e-jazah.id/msp/tlscacerts/tlsca.e-jazah.id-cert.pem -C main-channel -n basic --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer0.org1.e-jazah.id/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.e-jazah.id/peers/peer1.org1.e-jazah.id/tls/ca.crt -c '{"function":"InitLedger","Args":[]}'
 
@@ -25,4 +25,4 @@ export CORE_PEER_ADDRESS=localhost:7051
 # peer chaincode query -C main-channel -n basic -c '{"Args":["GetAllIjazah"]}' | jq 
 # peer chaincode query -C main-channel -n basic -c '{"Args":["ReadAsset", "asset1"]}' | jq 
 
-peer chaincode query -C main-channel -n basic -c '{"Args":["GetUserById", "itb@gmail.com"]}' | jq 
+# peer chaincode query -C main-channel -n basic -c '{"Args":["GetUserById", "itb@gmail.com"]}' | jq 
